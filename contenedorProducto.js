60b622e651001764044df943c6278b7fb309ac86
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-06-17 13:29:04 -0300

    desafioClase16

---
+++ contenedorProducto.js
@@ -0,0 +1,145 @@
+// DESAFIO CLASE 16 SQL
+
+const fs = require("fs");
+const express = require("express");
+const {engine} = require("express-handlebars");
+const {Router} = express;
+const router = Router();
+const multer = require("multer");
+const storage = multer({destinantion: "/upload"});
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+const Knex = require('knex').default;
+
+const {Server: HttpServer} = require('http');
+const {Server: SocketServer} = require('socket.io');
+
+const messages = [];
+
+const httpServer = new HttpServer(app);
+const socketServer = new SocketServer(httpServer);
+
+
+const optionsMySQL = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'ecommerce'
+};
+
+class Contenedor {
+    constructor(options, tabla) {
+        this.knex = Knex({
+            client: 'mysql2',
+            connection: options
+        });
+        this.tabla = tabla;
+    }
+
+    /**
+     * @param {json} producto
+     * Metodo para guardar un producto. Al terminar de grabar, muestra por pantalla el ID del producto agregado.
+     */
+
+    async metodoSave(producto) {
+        const contenido = this.knex.select('*').from('articulos');
+        producto.id = contenido.length + 1;
+        await this.knex('articulos').insert({
+            titulo: producto.title,
+            thumbnail: producto.thumbnail,
+            price: producto.price
+        })
+
+        console.log("El Id del Producto es " + producto.id);
+    }
+
+    /**
+     * Metodo para obtener todos los productos
+     */
+    // async getAll() {
+    //     try {
+    //         const contenido = await this.knex.select('*').from(this.tabla)
+    //             .then((filas) => console.log(filas));
+    //         // await this.knex.destroy();
+    //
+    //         // console.log(contenido);
+    //         return contenido;
+    //     } catch (error) {
+    //         console.log("Error en getAll", error);
+    //         return [];
+    //     }
+    // }
+
+    async getAll() {
+            const contenido = await this.knex.select('*').from(this.tabla);
+            return contenido;
+    }
+}
+
+// ------------------------ // CODIGO DEL SERVER  // ---------------- // ---------------- // ---------------- // ---------------- //
+
+// Middleware para parsear el Body. Sin esto no obtenemos el Body. SIEMPRE QUE USAMOS POST HAY QUE USARLO.
+// El body llega como strings. Lo que hace el middleware es transformarlo en JSON y mandarlo a la funcion que debe controlarlo.
+app.use(express.json());
+// Hace lo mismo pero con dato de formularios. Un form en HTML viene en forma de URL encoded y esto lo trasnforma en formulario.
+app.use(express.urlencoded({extended: true}));
+
+// Va a buscar en la carpeta PUBLIC si existe el archivo buscado.
+app.use(express.static("public"));
+
+// ROUTER
+app.use("/api", router);
+
+// Views Engine
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+    })
+);
+
+app.set("views", "./hbs_views");
+app.set("view engine", "hbs");
+
+app.get("/", async (req, res, next) => {
+    const productos = new Contenedor(optionsMySQL, 'articulos');
+    const showProductos = await productos.getAll();
+    res.render("main", {showProductos});
+});
+
+const productoSubido = storage.fields([
+    {title: "title", thumbnail: "thumbnail", price: "price"},
+]);
+
+app.post('/', productoSubido, async (req, res) => {
+    let produc = new Contenedor(optionsMySQL, 'articulos');
+    if (
+        req.body.title === "" ||
+        req.body.price === "" ||
+        req.body.thumbnail === ""
+    ) {
+        res.status(400).send({
+            error: "No se pudo cargar el producto. Complete los campos vacios.",
+        });
+    } else {
+        await produc.metodoSave(req.body);
+        res.redirect(`http://localhost:${PORT}`);
+    }
+})
+
+
+socketServer.on('connection', (socket) => {
+    socket.emit('messages', messages);
+
+    socket.on('new_message', (mensaje) => {
+        messages.push(mensaje);
+        socketServer.sockets.emit('messages', messages);
+    });
+
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Corriendo server en el puerto ${PORT}!`);
+});
